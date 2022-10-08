7b15dc0dce5c4e5458e88b1fd37914a776237b38
felix <felix0351z@gmail.com>
2022-10-08 13:21:56 +0200

    Installed FileHandler and added todos

--- src/main/kotlin/de/felix0351/Application.kt
+++ src/main/kotlin/de/felix0351/Application.kt
@@ -3,13 +3,31 @@ package de.felix0351
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import de.felix0351.plugins.*
+import de.felix0351.utils.FileHandler
+
+
+//TODO Beispiel config.yaml kommentieren
+
+//TODO DatatenbankService erstellen
+//TODO DAO Interface
+//TODO DAO Implementation
+//TODO DAO Testing
+
+//TODO Authentifizierung (LDAP vs Session?)
+//TODO Routen definieren
 
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureSecurity()
-        configureSerialization()
-        configureRouting()
-    }.start(wait = true)
+    //Load all configurations for the server
+    FileHandler.load()
+
+    runServer(FileHandler.configuration.port)
 }
+
+fun runServer(port: Int)  = embeddedServer(Netty, port = port, host = "0.0.0.0") {
+    configureSecurity()
+    configureSerialization()
+    configureRouting()
+}.start(wait = true)
+
 
