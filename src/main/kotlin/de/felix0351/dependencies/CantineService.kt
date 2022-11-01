91ce30940b5c28e10d58ccfa7abbc178ae54cd73
felix <felix0351z@gmail.com>
2022-11-01 22:22:03 +0100

    Implemented checkUserCredentials

--- src/main/kotlin/de/felix0351/dependencies/CantineService.kt
+++ src/main/kotlin/de/felix0351/dependencies/CantineService.kt
@@ -1,17 +1,18 @@
 package de.felix0351.dependencies
 
+import de.felix0351.db.query
+
 class CantineService(
-    private val authRepository: AuthenticationRepository,
-    private val contentRepository: ContentRepository
+    private val authRepo: AuthenticationRepository,
+    private val contentRepo: ContentRepository
     ) {
 
 
-    suspend fun checkUserCredentials(username: String, password: String): Boolean {
-        TODO("Implement")
+    suspend fun checkUserCredentials(username: String, password: String): Boolean = query {
+        val user = authRepo.getUserByUsername(username) ?: return@query false
+
+        user.password == password
     }
-
-
-
 
 
 
