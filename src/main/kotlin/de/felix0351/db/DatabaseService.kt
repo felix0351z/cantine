85573d03abe88bad2c62599fae607c77a3e18dd1
felix <felix0351z@gmail.com>
2022-11-01 22:19:57 +0100

    Finished DatabaseService

--- src/main/kotlin/de/felix0351/db/DatabaseService.kt
+++ src/main/kotlin/de/felix0351/db/DatabaseService.kt
@@ -2,23 +2,40 @@ package de.felix0351.db
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import de.felix0351.models.ConfigFile
+import de.felix0351.models.tables.Meals
+import de.felix0351.models.tables.News
+import de.felix0351.models.tables.UserSessions
+import de.felix0351.models.tables.Users
+import de.felix0351.utils.FileHandler
 import de.felix0351.utils.fail
+import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseService {
+
+    private val TABLES = listOf(
+        Users, UserSessions,
+        News,
+        Meals.Categories, Meals.Meals, Meals.Orders, Meals.SelectionGroups
+        )
     
-    fun init(config: ConfigFile) {
+    fun init() {
         try {
-            val type = ConnectionFactory.fromConfig(config)
+            val type = ConnectionFactory.fromConfig(FileHandler.configuration)
             val db = when(type) {
                 is Connection.MySQL -> connectHikari(type)
                 is Connection.SQLite -> connectSQLite(type)
             }
 
             transaction {
-                //TODO Create tables and initialize them
+                // Erstelle alle benötigten Tabellen für die Repos
+                TABLES.forEach { table ->
+                    SchemaUtils.create(table)
+                }
             }
 
 
@@ -44,6 +61,13 @@ object DatabaseService {
         val dataSource = HikariDataSource(config)
         return Database.connect(dataSource)
     }
-    
 
 }
+
+/**
+ * Easy access function to make an async sql statement
+ */
+suspend fun <T> query(statement: suspend Transaction.() -> T): T =
+    newSuspendedTransaction(Dispatchers.IO) {
+        statement()
+    }
