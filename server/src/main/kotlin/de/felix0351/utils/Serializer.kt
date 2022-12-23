264c4714e5a14ed6dfa70366d12f13265ff56905
Felix Zimmermann <felix0351z@gmail.com>
2022-12-23 15:23:05 +0100

    Added InstantSerializer and removed DateTimeSerializer

--- server/src/main/kotlin/de/felix0351/utils/Serializer.kt
+++ server/src/main/kotlin/de/felix0351/utils/Serializer.kt
@@ -9,45 +9,29 @@ import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import java.time.Instant
 import java.util.UUID
 
 /**
- * Serializer for the LocalDateTime object in Java
+ * Serializer for the Instant object in Java
  * If the time is null and not provided, it will take the current time
  *
- * @property FORMAT Format as String ("yyyy-MM-dd:HH:mm:ss")
- *
  */
-object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
-
-    private const val FORMAT = "yyyy-MM-dd:HH:mm:ss"
-    private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern(FORMAT)
-
-
+object InstantSerializer : KSerializer<Instant> {
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(
-        serialName = "time",
-        kind = PrimitiveKind.STRING
-    )
+        serialName = "Instant",
+        kind = PrimitiveKind.LONG)
 
     @OptIn(ExperimentalSerializationApi::class)
-    override fun deserialize(decoder: Decoder): LocalDateTime {
-        val str = decoder.decodeNullableSerializableValue(String.serializer().nullable)
+    override fun deserialize(decoder: Decoder): Instant {
+        val long = decoder.decodeNullableSerializableValue(Long.serializer().nullable)
 
-        return if (str == null)  {
-            LocalDateTime.now()
-        }
-        else  {
-            LocalDateTime.parse(str, formatter)
-        }
+        return if (long == null) Instant.now()
+        else Instant.ofEpochMilli(decoder.decodeLong())
     }
 
-    override fun serialize(encoder: Encoder, value: LocalDateTime) {
-        val str = value.format(formatter)
-        encoder.encodeString(str)
-    }
-
+    override fun serialize(encoder: Encoder, value: Instant) =
+        encoder.encodeLong(value.toEpochMilli())
 }
 
 
