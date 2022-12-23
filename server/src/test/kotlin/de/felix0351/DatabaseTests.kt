b2e0ea5d46e2d8a5386c6652e2664949f68c896b
Felix Zimmermann <felix0351z@gmail.com>
2022-12-23 15:28:59 +0100

    Added repository tests

--- server/src/test/kotlin/de/felix0351/DatabaseTests.kt
+++ server/src/test/kotlin/de/felix0351/DatabaseTests.kt
@@ -1,23 +1,30 @@
 package de.felix0351
 
 import de.felix0351.db.MongoDBConnection
+import de.felix0351.dependencies.AuthenticationRepository
 import de.felix0351.dependencies.AuthenticationRepositoryMongoDB
-import de.felix0351.models.errors.Response
-import de.felix0351.models.objects.Auth
-import de.felix0351.models.errors.ErrorCode
+import de.felix0351.dependencies.ContentRepository
+import de.felix0351.dependencies.ContentRepositoryMongoDB
 import de.felix0351.utils.FileHandler
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotEquals
+
+fun getConnection(): MongoDBConnection {
+    FileHandler.load()
+    return MongoDBConnection()
+}
+
+fun getAuthRepo(): AuthenticationRepository {
+    return AuthenticationRepositoryMongoDB(getConnection())
+}
+
+fun getContentRepo(): ContentRepository {
+    return ContentRepositoryMongoDB(getConnection())
+}
 
 class DatabaseTests {
 
-    private fun getConnection(): MongoDBConnection {
-        FileHandler.load()
-        return MongoDBConnection()
-    }
 
     @Test
     fun testConnection() {
@@ -30,7 +37,7 @@ class DatabaseTests {
         val con = getConnection()
 
         val lists = runBlocking {
-            con.call {
+            con.callToDatabase {
                 it.listCollectionNames()
             }
         }
@@ -43,55 +50,14 @@ class DatabaseTests {
        val con = getConnection()
 
        runBlocking {
-           con.call { it.listCollectionNames() }
+           con.callToDatabase { it.listCollectionNames() }
            println("Call 1 executed")
            delay(15*1000)
 
            // Disconnect lan manually
-           con.call { it.listCollectionNames() }
+           con.callToDatabase { it.listCollectionNames() }
        }
 
    }
-
-    @Test
-    fun addUser() = runBlocking {
-        val user = Auth.User(
-            id = null,
-            username = "felix0351",
-            name = "Felix Zimmermann",
-            permissionLevel = Auth.PermissionLevel.ADMIN,
-            credit = "fdgfgdf",
-            hash = "blabla"
-
-        )
-
-
-        val con = getConnection()
-        val repo = AuthenticationRepositoryMongoDB(con)
-
-        val response = repo.addUser(user)
-        assertEquals(response, Response.Ok)
-
-    }
-
-    @Test
-    fun getUser() = runBlocking {
-        val con = getConnection()
-        val repo = AuthenticationRepositoryMongoDB(con)
-
-        val user = repo.getUserByUsername("felix0351")
-
-        assertNotEquals(user, null)
-    }
-
-    @Test
-    fun updatePermissionStatus() = runBlocking {
-        val con = getConnection()
-        val repo = AuthenticationRepositoryMongoDB(con)
-
-        val response = repo.updatePermissionLevel("felix0351", Auth.PermissionLevel.ADMIN)
-
-        assertEquals(response, Response.Error(ErrorCode.SameValue))
-    }
 
 }
