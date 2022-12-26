f6b1fb2142d2a63fe46bc8176cf8565d6a109e5a
Julian1408 <93348523+Julian1408@users.noreply.github.com>
2022-12-26 10:36:46 +0100

    added routes

--- server/src/main/kotlin/de/felix0351/routes/ContentRoutes.kt
+++ server/src/main/kotlin/de/felix0351/routes/ContentRoutes.kt
@@ -2,8 +2,44 @@ package de.felix0351.routes
 
 import io.ktor.server.application.*
 
+/**
+ * get all meals
+ */
+fun Route.meal() {
 
+}
 
+/**
+ * get all reports
+ */
+fun Route.getReports() {
+
+}
+
+/**
+ * create a new report
+ */
+fun Route.report(){
+
+}
+
+/**
+ * create a new order
+ */
+fun Route.order(){
+
+}
+
+fun Route.payment(){
+
+}
+
+/**
+ * get user information
+ */
+fun Route.user(){
+
+}
 
 fun Application.contentRoutes() {
 
