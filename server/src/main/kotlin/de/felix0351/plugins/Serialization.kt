7e6870c38acbf364e6b4e4ced6d3599d36f900b6
Felix Zimmermann <felix0351z@gmail.com>
2022-12-27 12:18:38 +0100

    Configured content negotiation

--- server/src/main/kotlin/de/felix0351/plugins/Serialization.kt
+++ server/src/main/kotlin/de/felix0351/plugins/Serialization.kt
@@ -3,10 +3,17 @@ package de.felix0351.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.application.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(
+            Json {
+                prettyPrint = true
+                isLenient = true
+            }
+
+        )
     }
 
 
