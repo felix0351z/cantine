c666bfa3adc3bc06a06c86897397ff4f7e4bebbb
Julian1408 <93348523+Julian1408@users.noreply.github.com>
2023-02-27 18:49:34 +0100

    Added generic functions to LocalDataStore

--- app/shared/src/commonMain/kotlin/de/juliando/app/data/LocalDataStore.kt
+++ app/shared/src/commonMain/kotlin/de/juliando/app/data/LocalDataStore.kt
@@ -10,26 +10,8 @@ import io.ktor.http.*
 
 interface LocalDataStore {
 
-    fun storeMeals(meals: List<Content.Meal>?)
-    fun getMeals(): List<Content.Meal>?
-
-    fun storeReports(reports: List<Content.Report>?)
-    fun getReports(): List<Content.Report>?
-
-    fun storeCategories(categories: List<Content.Category>?)
-    fun getCategories(): List<Content.Category>?
-
-    fun storeSelections(selections: List<Content.Selection>?)
-    fun getSelections(): List<Content.Selection>?
-
-    fun storeOrders(orders: List<Content.Order>?)
-    fun getOrders(): List<Content.Order>?
-
-    fun storePurchases(selections: List<Auth.Payment>?)
-    fun getPurchases(): List<Auth.Payment>?
-
     /**
-     * Functions to store and get the Cookie
+     * Functions to store and get a Cookie
      */
     fun storeCookie(cookie: Cookie?)
     fun getCookie(): Cookie?
