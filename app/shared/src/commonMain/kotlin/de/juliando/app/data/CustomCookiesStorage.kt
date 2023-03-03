d654cf53de36b4f16b23be6b9bbb998234e31e6e
Julian1408 <93348523+Julian1408@users.noreply.github.com>
2023-03-03 21:03:09 +0100

    Added generic functions to ServerDataSource

--- app/shared/src/commonMain/kotlin/de/juliando/app/data/CustomCookiesStorage.kt
+++ app/shared/src/commonMain/kotlin/de/juliando/app/data/CustomCookiesStorage.kt
@@ -8,14 +8,14 @@ class CustomCookiesStorage(
     private val localStorage: LocalDataStoreImpl = LocalDataStoreImpl()
 ) : CookiesStorage {
     /**
-     * Sets a [cookie] for the specified host.
+     * Stores the cookie in local storage
      */
     override suspend fun addCookie(requestUrl: Url, cookie: Cookie) {
         localStorage.storeCookie(cookie)
     }
 
     /**
-     * Gets a map of [String] to [Cookie] for a specific host.
+     * Gets the cookie from local storage
      */
     override suspend fun get(requestUrl: Url): List<Cookie> {
         val cookie: Cookie? = localStorage.getCookie()
