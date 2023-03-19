454be11bef2629fc31fc04ae5fe46cc5c4ce6b80
Felix Zimmermann <felix0351z@gmail.com>
2023-03-19 21:02:47 +0100

    Added first draft for the report element

--- app/androidCantine/src/main/java/de/juliando/app/android/ui/home/HomeScreen.kt
+++ app/androidCantine/src/main/java/de/juliando/app/android/ui/home/HomeScreen.kt
@@ -1,25 +1,32 @@
 package de.juliando.app.android.ui.home
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import android.util.Log
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
 import de.juliando.app.android.CantineTheme
+import de.juliando.app.android.ui.utils.DataState
+import de.juliando.app.models.objects.Content
+
+import org.koin.androidx.compose.koinViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen() {
+fun HomeScreen(viewModel: HomeViewModel = koinViewModel()) {
 
     val d = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+    val reports by viewModel.reports.collectAsStateWithLifecycle()
+    val reportsListState = rememberLazyListState()
 
 
     Scaffold(
@@ -63,6 +70,22 @@ fun HomeScreen() {
         }
         
     ) {
+        when(reports) {
+            is DataState.Success<*> -> {
+                val items: List<Content.Report> = (reports as DataState.Success<*>).value as List<Content.Report>
+
+                Log.d("fdbgfd", "Success")
+
+                /*ReportList(
+                    items = (reports as DataState.Success<*>).value as List<Content.Report>,
+                    state = reportsListState
+                )*/
+            }
+            else -> {
+                System.out.println("No Success")
+                //TODO
+            }
+        }
 
     }
 
--- app/androidCantine/src/main/java/de/juliando/app/android/ui/home/ReportComponents.kt
+++ app/androidCantine/src/main/java/de/juliando/app/android/ui/home/ReportComponents.kt
@@ -1,26 +1,66 @@
 package de.juliando.app.android.ui.home
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.bumptech.glide.Glide
 
 import com.bumptech.glide.RequestManager
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
+import com.bumptech.glide.integration.compose.GlideLazyListPreloader
 import de.juliando.app.android.CantineTheme
 
 import de.juliando.app.models.objects.Content
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun ReportList(
+    items: List<Content.Report>,
+    state: LazyListState,
+) {
+    val requestManager = Glide.with(LocalContext.current)
+    val size = Size(200f, 100f)
+
+    GlideLazyListPreloader(
+        state = state,
+        data = items,
+        size = size,
+        numberOfItemsToPreload = 2,
+    ) { item, requestBuilder ->
+        requestBuilder.load(item.picture)
+    }
+
+    LazyColumn(
+        state = state
+    ) {
+        items(items) {
+            ReportCard(item = it, requestManager)
+        }
+
+    }
+
+
+    
+}
 
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun ReportCard(
     item: Content.Report,
-    //requestManager: RequestManager
+    requestManager: RequestManager,
 ) {
     Column(
         modifier = Modifier.size(305.dp, 200.dp),
@@ -28,12 +68,20 @@ fun ReportCard(
     ) {
         // Picture
         GlideImage(
-            modifier = Modifier.fillMaxWidth().height(100.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
             model = item.picture,
             contentDescription = item.title
         ) {
             it
                 .error(item.picture)
+                .thumbnail(
+                    requestManager
+                        .asDrawable()
+                        .load(item.picture)
+                        .override(200, 100)
+                )
                 .fitCenter()
         }
         // Title
@@ -50,12 +98,3 @@ fun ReportCard(
 
 }
 
-@Preview
-@Composable
-fun ReportCardPreview() {
-    val item = ReportCard(item = )
-
-    ReportCard(
-        item = ,
-    )
-}
