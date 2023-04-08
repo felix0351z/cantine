2ba62ebab909a85a5c2e3671c2178c4483f531df
Felix Zimmermann <felix0351z@gmail.com>
2023-04-08 11:12:33 +0200

    Added comments for the ios part

--- app/shared/src/iosMain/kotlin/de/juliando/app/data/KtorIosConfiguration.kt
+++ app/shared/src/iosMain/kotlin/de/juliando/app/data/KtorIosConfiguration.kt
@@ -2,12 +2,20 @@ package de.juliando.app.data
 
 import io.ktor.client.*
 import io.ktor.client.engine.darwin.*
+import io.ktor.client.engine.darwin.certificates.*
 
 /**
  *  Returns a pre-configured ktor httpclient for the ios platform
  * @param config Config for all platforms
+ *
+ * See https://api.ktor.io/ktor-client/ktor-client-darwin/io.ktor.client.engine.darwin.certificates/-certificate-pinner/index.html
  */
 actual fun httpClient(config: HttpClientConfig<*>.() -> Unit) = HttpClient(Darwin) {
+    engine {
+        val builder = CertificatePinner.Builder().add( "",  "")
+        handleChallenge(builder.build())
+    }
+
     //TODO: Configure SSL
     config()
 }
--- app/shared/src/iosMain/kotlin/de/juliando/app/utils/IosDisplayFormatting.kt
+++ app/shared/src/iosMain/kotlin/de/juliando/app/utils/IosDisplayFormatting.kt
@@ -1,6 +1,6 @@
 package de.juliando.app.utils
 
-
+// https://github.com/JetBrains/kotlin-native/issues/2858
 actual fun toCurrencyString(amount: Float): String {
     TODO()
 }
